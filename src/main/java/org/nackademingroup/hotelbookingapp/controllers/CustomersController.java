9e09c6c0acd2a3f757a8b252f83dae9015edc7cf
mhultjavad24 <mhultjavad24@proton.me>
2025-05-15 14:16:55 +0200

    Implement customers view for list and detail

--- src/main/java/org/nackademingroup/hotelbookingapp/controllers/CustomersController.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/controllers/CustomersController.java
@@ -1,15 +1,27 @@
 package org.nackademingroup.hotelbookingapp.controllers;
 
 import org.nackademingroup.hotelbookingapp.models.Customer;
+import org.nackademingroup.hotelbookingapp.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
 @Controller
 public class CustomersController {
 
+    @Autowired
+    private CustomerService customerService;
+
     @GetMapping("/customers")
     public String customer(Model model) {
+        // Get mock customers from the service
+        List<Customer> customers = customerService.getMockCustomers();
+        
+        // Add customers to the model to be used in the view
+        model.addAttribute("customers", customers);
+        
         return "customers";
     }
 }
