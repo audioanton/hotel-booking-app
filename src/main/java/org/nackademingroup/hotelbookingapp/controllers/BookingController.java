a89239398fd5be8528c8f1e6a6d2ec0df8a0b712
viktorjonasson <viktorjonasson@gmail.com>
2025-05-15 15:41:57 +0200

    Bookings page with mockup data from BookingService.

--- src/main/java/org/nackademingroup/hotelbookingapp/controllers/BookingController.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/controllers/BookingController.java
@@ -1,8 +1,24 @@
 package org.nackademingroup.hotelbookingapp.controllers;
 
-import org.nackademingroup.hotelbookingapp.models.Room;
-import org.springframework.web.bind.annotation.RestController;
+import org.nackademingroup.hotelbookingapp.models.Booking;
+import org.nackademingroup.hotelbookingapp.services.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping("/bookings")
+    public String booking(Model model) {
+        List<Booking> bookings = bookingService.getMockBookings();
+        model.addAttribute("bookings", bookings);
+        return "bookings";
+    }
 }
--- src/main/java/org/nackademingroup/hotelbookingapp/services/BookingService.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/services/BookingService.java
@@ -1,12 +1,33 @@
 package org.nackademingroup.hotelbookingapp.services;
 
+import org.nackademingroup.hotelbookingapp.models.Booking;
 import org.nackademingroup.hotelbookingapp.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BookingService {
 
     @Autowired
     private BookingRepository bookingRepository;
+
+    public List<Booking> getMockBookings(){
+        List<Booking> mockBookings = new ArrayList<>();
+
+        mockBookings.add(Booking.builder()
+                .id(1L)
+                .start_date(LocalDate.parse("2025-05-12"))
+                .end_date(LocalDate.parse("2025-05-14")).build());
+
+        mockBookings.add(Booking.builder()
+                .id(2L)
+                .start_date(LocalDate.parse("2025-05-16"))
+                .end_date(LocalDate.parse("2025-05-17")).build());
+
+    return mockBookings;
+    }
 }
