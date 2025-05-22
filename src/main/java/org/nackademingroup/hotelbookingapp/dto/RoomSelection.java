c2ac379e9948644072a8a3d9023d4d869521b6b2
anton <antonlarsson.musik@gmail.com>
2025-05-22 11:19:56 +0200

    some fixes

--- src/main/java/org/nackademingroup/hotelbookingapp/dto/RoomSelection.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/dto/RoomSelection.java
@@ -1,9 +1,13 @@
 package org.nackademingroup.hotelbookingapp.dto;
 
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -12,5 +16,9 @@ import lombok.NoArgsConstructor;
 public class RoomSelection {
     private int roomId;
     private int totalGuests;
-    private int guestId;
+    private Long customerId;
+    @Temporal(TemporalType.DATE)
+    private LocalDate startDate;
+    @Temporal(TemporalType.DATE)
+    private LocalDate endDate;
 }
