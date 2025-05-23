c21317fbaf379d8799a2895188d9dff61800e360
anton <antonlarsson.musik@gmail.com>
2025-05-23 08:55:41 +0200

    Got dates and guests to work

--- src/main/java/org/nackademingroup/hotelbookingapp/dto/RoomSearch.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/dto/RoomSearch.java
@@ -6,8 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Data
 @Builder
@@ -19,4 +22,5 @@ public class RoomSearch {
     @Temporal(TemporalType.DATE)
     private LocalDate endDate;
     private int totalGuests;
+
 }
