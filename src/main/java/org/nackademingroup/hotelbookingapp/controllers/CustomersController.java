5da8b972f966a12cb3854f3aa3d027911b46c5ac
mhultjavad24 <mhultjavad24@proton.me>
2025-05-24 00:59:24 +0200

    Validate and show validation feedback for user on Customer Flow

--- src/main/java/org/nackademingroup/hotelbookingapp/controllers/CustomersController.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/controllers/CustomersController.java
@@ -6,11 +6,15 @@ import org.nackademingroup.hotelbookingapp.services.service_implementations.Cust
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.validation.Valid;
+
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,17 +52,30 @@ public class CustomersController {
     }
 
     @PostMapping("/customers/create")
-    public String createCustomer(Customer customer) {
-        customerService.createCustomer(customer);
-        return "redirect:/customers";
+    public String createCustomer(@Valid CustomerDto customerDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("error", bindingResult.getFieldError().getDefaultMessage());
+            model.addAttribute("customer", customerDto);
+            return "redirect:/customers/new";
+        } else {
+            customerService.createCustomer(customerService.toCustomer(customerDto));
+            return "redirect:/customers";
+        }
     }
 
     @PostMapping("/customers/{id}")
-    public String updateCustomer(@PathVariable("id") Long id, Customer customer) {
-        // TODO: Validation?
-        customer.setId(id);
-        customerService.updateCustomer(customer);
-        return "redirect:/customers";
+    public String updateCustomer(@PathVariable("id") Long id, @Valid CustomerDto customerDto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            customerDto.setId(id);
+            redirectAttributes.addFlashAttribute("error", bindingResult.getFieldError().getDefaultMessage());
+            model.addAttribute("customer", customerDto);
+            return "redirect:/customers/" + id;
+        } else {
+            Customer customer = customerService.toCustomer(customerDto);
+            customer.setId(id);
+            customerService.updateCustomer(customer);
+            return "redirect:/customers";
+        }
     }
 
     @PostMapping("/customers/{id}/delete")
--- src/main/java/org/nackademingroup/hotelbookingapp/dto/CustomerDto.java
+++ src/main/java/org/nackademingroup/hotelbookingapp/dto/CustomerDto.java
@@ -1,5 +1,8 @@
 package org.nackademingroup.hotelbookingapp.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,5 +14,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CustomerDto {
     private Long id;
-    private String name, phoneNumber;
+    @NotNull(message = "Customer must have a name")
+    @Size(min = 1, max = 50, message = "Customer name must be between 1 and 50 characters")
+    private String name;
+    @NotNull(message = "Customer must have a phone number")
+    @Size(min = 1, max = 15, message = "Customer phone number must be between 1 and 15 characters")
+    private String phoneNumber;
 }
