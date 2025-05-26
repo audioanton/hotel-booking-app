5b6f8ccf06520075c41013cd7965a4582ab1a765
viktorjonasson <viktorjonasson@gmail.com>
2025-05-26 14:25:39 +0200

    Test for toCustomerDto

--- src/test/java/org/nackademingroup/hotelbookingapp/services/service_implementations/CustomerServiceImpTest.java
+++ src/test/java/org/nackademingroup/hotelbookingapp/services/service_implementations/CustomerServiceImpTest.java
@@ -1,12 +1,16 @@
 package org.nackademingroup.hotelbookingapp.services.service_implementations;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.nackademingroup.hotelbookingapp.dto.CustomerDto;
 import org.nackademingroup.hotelbookingapp.models.Customer;
 import org.nackademingroup.hotelbookingapp.repositories.CustomerRepository;
+import org.nackademingroup.hotelbookingapp.services.service_interfaces.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -17,28 +21,40 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Transactional
+@Rollback
 class CustomerServiceImpTest {
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    private CustomerServiceImp customerServiceImp;
+    private CustomerService customerService;
     @Autowired
     private CustomerRepository customerRepository;
 
-    @BeforeEach
+    Customer customer = Customer.builder().name("Bingo").phoneNumber("1").build();
+    CustomerDto customerDto = CustomerDto.builder().name("Bingo").phoneNumber("1").build();
+
+    /*@BeforeEach
     public void setup() {
         customerRepository.deleteAll();
         customerRepository.saveAll(
                 List.of(
-                        Customer.builder().name("Bingo").id(1L).phoneNumber("1").build(),
-                        Customer.builder().name("Ringo").id(2L).phoneNumber("2").build(),
-                        Customer.builder().name("Dingo").id(3L).phoneNumber("3").build()
+                        Customer.builder().name("Bingo").phoneNumber("1").build(),
+                        Customer.builder().name("Ringo").phoneNumber("2").build(),
+                        Customer.builder().name("Dingo").phoneNumber("3").build()
                 )
         );
-    }
+    }*/
 
     @Test
+    void toCustomerDto() {
+        CustomerDto actualDto = customerService.toCustomerDto(customer);
+        assertEquals(customer.getName(), actualDto.getName());
+        assertEquals(customer.getPhoneNumber(), actualDto.getPhoneNumber());
+    }
+
+    /*@Test
     void getCustomerById() {
         fail();
     }
@@ -51,5 +67,7 @@ class CustomerServiceImpTest {
     @Test
     void toCustomerDto() {
         fail();
-    }
+    }*/
+
+
 }
